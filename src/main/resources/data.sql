74d269f0ad9493e7b87542a5e1e81920c7c00f87
K Hudson <kay.hudson@galvanize.com>
2021-01-20 06:29:15 -0800

    Add sandwiches.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO sandwiches (name) VALUES ('Ham and Cheese');
+INSERT INTO sandwiches (name) VALUES ('Turkey and Swiss');
+INSERT INTO sandwiches (name) VALUES ('Veggie Deluxe');
+INSERT INTO sandwiches (name) VALUES ('Kale and Lime');
--- src/test/java/com/galvanize/dock_sandwich/SandwichControllerTest.java
+++ src/test/java/com/galvanize/dock_sandwich/SandwichControllerTest.java
@@ -1,13 +1,23 @@
 package com.galvanize.dock_sandwich;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SandwichControllerTest {
+    @Autowired
+    MockMvc mvc;
 
     @Test
-    void testGetPage() {
-
+    void testGetAllSandwiches() throws Exception {
+        mvc.perform(get("/sandwiches"))
+                .andExpect(status().isOk());
     }
 }
